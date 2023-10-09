e2ae6ea1c84545c14940425a9731167b1dae7950
Alexey Veselov <ukiroot@gmail.com>
2023-10-09 21:37:24 +0400

    7_devops/k8s/kubernetes/centos7-kubernetes-one-node.bash: update

--- 7_devops/k8s/kubernetes/centos7-kubernetes-one-node.bash
+++ 7_devops/k8s/kubernetes/centos7-kubernetes-one-node.bash
@@ -22,8 +22,8 @@ precondition_known_issues () {
    Details: https://github.com/kubernetes/kubeadm/issues/1062
    '
    cat <<EOF > /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 0
-net.bridge.bridge-nf-call-iptables = 0
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
 EOF
    sysctl --system
 
