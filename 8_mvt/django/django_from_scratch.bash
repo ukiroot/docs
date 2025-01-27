b92f50afeb3892e052aa9dc608bece9f6d6801b0
Alexey Veselov <ukiroot@gmail.com>
2025-01-27 21:49:51 +0400

    add 8_mvt/django/django_from_scratch.bash
    Document '8_mvt/django/django_from_scratch.bash' the sequential development
    of Django from scratch

---
+++ 8_mvt/django/django_from_scratch.bash
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+#Step preparation
+export PYTHONDONTWRITEBYTECODE=1
+rm -rfv ~/Documents/django_from_scratch
+mkdir -p ~/Documents/django_from_scratch
+pushd ~/Documents/django_from_scratch
+
+
+#Step 0: init empty git project
+git init 
+git config user.name "Alexey Veselov"
+git config user.email "ukiroot@gmail.com"
+cat > .gitignore << EOF
+.gitignore
+.django_venv
+EOF
+
+
+#Step 1: init virtual ENV
+python3 -m venv .django_venv
+source .django_venv/bin/activate
+
+
+#Step 2: add minimal necessary requirements for Django and install them
+cat > requirements.txt << EOF
+django==5.1.5
+EOF
+python3 -m pip install -r requirements.txt
+git add requirements.txt
+git commit -a -m "Step 2: add minimal necessary requirements for Django and install them"
+
+
+#Step 3: init new  django project with name 'django_project' 
+django-admin startproject django_project .
+git add django_project/ manage.py
+git commit -a -m "Step 3: init new  django project with name 'django_project'"
+
+
+#Step 4: create simple views based on HttpResponse
+cat > django_project/views.py << "EOF"
+from django.http import HttpResponse
+
+def api_hello_world(request):
+   return HttpResponse("Hello World.")
+
+def api_request_info(request):
+   import json
+   response_data = {
+      "request.method": request.method,
+      "request.path": request.path
+   }
+   response_body = json.dumps(response_data)
+   return HttpResponse(response_body)
+EOF
+sed -i \
+    -e '18 a\from . import views' \
+    -e "21 a\    path('api/hello_world', views.api_hello_world),\n    path('api/hello_url', views.api_request_info)," \
+    django_project/urls.py
+git add django_project/views.py
+git commit -a -m "Step 4: create simple views based on HttpResponse"
+
+
+#Step 5: add to 'views.py' new views based on render 'templating' 
+mkdir templates
+cat > templates/root.html << "EOF"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>root</title>
+</head>
+<body>
+    <div>
+        <h1>root</h1>
+        <p>Current page link <a href="/">Root</a> page.</p>
+        <p>Check out my <a href="/info">Info</a> page.</p>
+    </div>
+</body>
+</html>
+EOF
+cat > templates/info.html << "EOF"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info</title>
+</head>
+<body>
+    <div>
+        <h1>Info</h1>
+        <p>Current page link <a href="/info">Info</a> page.</p>
+        <p>Check out my <a href="/">Root</a> page.</p>
+    </div>
+</body>
+</html>
+EOF
+cat >>  django_project/views.py << "EOF"
+
+########################################
+########################################
+
+from django.shortcuts import render
+
+
+def root(request):
+    return render(request, 'root.html')
+
+
+def info(request):
+    return render(request, 'info.html')
+EOF
+sed -i "s/^        'DIRS': \[\],/        'DIRS': ['templates'],/"  django_project/settings.py
+sed -i \
+    -e "23 a\    path('', views.root),\n    path('info', views.info)," \
+    django_project/urls.py
+git add templates/
+git commit -a -m 'Step 5: add to 'views.py' new views based on render 'templating' '
+
+
+#Step 6: add static, css styles
+sed -i \
+    -e "12 a\import os" \
+    -e "118 a\STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]" \
+    django_project/settings.py
+mkdir -p static/css
+cat > static/css/style.css << "EOF"
+body {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Arial, sans-serif;
+}
+.content {
+    text-align: center;
+}
+EOF
+sed -i \
+    -e '2 a\{% load static %}' \
+    -e '6 a\    <link rel="stylesheet" href="{% static '"'"css/style.css"'"' %}">' \
+    templates/root.html
+sed -i \
+    -e '2 a\{% load static %}' \
+    -e '6 a\    <link rel="stylesheet" href="{% static '"'"css/style.css"'"' %}">' \
+    templates/info.html
+git add static
+git commit -a -m 'Step 6: add static, css styles'
+
+
+#Step 7: add static, javaScript code
+mkdir -p static/js/
+cat > static/js/main.js << "EOF"
+console.log('Just debug console message from ' + window.location.href)
+EOF
+sed -i \
+    -e '8 a\    <script src="{% static '"'"js/main.js"'"' %}" defer></script>' \
+    templates/root.html
+sed -i \
+    -e '8 a\    <script src="{% static '"'"js/main.js"'"' %}" defer></script>' \
+    templates/info.html
+git commit -a -m 'Step 7: add static, javaScript code'
+
+
+# #Step XXX:
+# python3 manage.py migrate
+# git add db.sqlite3
+# git commit -a -m "Third step: 'python3 manage.py migrate'"
+# 
+# 
+# #Step XXX:
+# DJANGO_SUPERUSER_USERNAME=django DJANGO_SUPERUSER_EMAIL=django@example.com DJANGO_SUPERUSER_PASSWORD=django python3 manage.py createsuperuser --noinput
+# git commit db.sqlite3 -m "XXX Step: Create superuser"
