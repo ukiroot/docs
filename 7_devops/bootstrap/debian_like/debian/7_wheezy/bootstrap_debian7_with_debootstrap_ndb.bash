90d05a39073bb762832a0cb0b8de545390cc9017
Alexey Veselov <ukiroot@gmail.com>
2025-02-01 14:33:33 +0000

    bootstrap_debian7_with_debootstrap_ndb.bash: update
    Boot process restored

--- 7_devops/bootstrap/debian_like/debian/7_wheezy/bootstrap_debian7_with_debootstrap_ndb.bash
+++ 7_devops/bootstrap/debian_like/debian/7_wheezy/bootstrap_debian7_with_debootstrap_ndb.bash
@@ -12,12 +12,9 @@ flock --exclusive /tmp/apt_from_docs.lock \
 mkdir -p /var/lib/libvirt/images/min_dist
 pushd /var/lib/libvirt/images/min_dist
 
+rm -fv debian_7.img
 qemu-img create debian_7.img 1G
-modprobe nbd max_part=15
-qemu-nbd -f raw  -c /dev/nbd0 debian_7.img || true
-
-
-fdisk /dev/nbd0 << "EOF"
+fdisk debian_7.img << "EOF"
 n
 p
 1
@@ -26,14 +23,20 @@ p
 a
 w
 EOF
-mkfs.ext4 /dev/nbd0p1
+
+modprobe nbd max_part=15
+qemu-nbd -f raw  -c /dev/nbd0 debian_7.img
+partprobe /dev/nbd0
+
+#use ext2 because the latest ext3/ext4 features may not be fully compatible with kernel 3.2.
+mkfs.ext2 /dev/nbd0p1
 mkdir -p  /mnt/debian_7
 mount -v /dev/nbd0p1 /mnt/debian_7
 
-debootstrap --verbose --include=sudo,nano,wget,grub-pc --arch amd64 wheezy /mnt/debian_7 http://archive.debian.org/debian/
+debootstrap --verbose --include=sudo,nano,wget,grub-pc,linux-image-amd64 --arch amd64 wheezy /mnt/debian_7 http://archive.debian.org/debian/
 
 cat > /mnt/debian_7/etc/fstab << "EOF"
-/dev/sda1       /               ext4        defaults        0       1
+/dev/sda1       /               ext2        defaults        0       1
 EOF
 
 cat > /mnt/debian_7/etc/apt/sources.list << "EOF"
@@ -46,7 +49,10 @@ EOF
 cat > /mnt/debian_7/root/postinst.sh << "EOF"
 #!/bin/bash
 
-apt-get update
+set -o xtrace
+set -o verbose
+set -o errexit
+
 
 passwd << "OEF"
 admin
@@ -54,17 +60,17 @@ admin
 OEF
 
 
-apt-get -y install linux-image-amd64
 #apt-get -y install firmware-linux firmware-ralink firmware-realtek
-apt-get clean
+#apt-get clean
 
 sed -i 's/^#T0:23.*/T0:23:respawn:\/sbin\/getty -L ttyS0 115200 vt100/' /etc/inittab
 sed -i 's/^#GRUB_TERMINAL.*/GRUB_TERMINAL="serial console"/' /etc/default/grub
 sed -i 's/^GRUB_CMDLINE_LINUX.*/GRUB_CMDLINE_LINUX="console=ttyS0"/' /etc/default/grub
 
-update-grub2
-grub-install /dev/nbd0 --modules="biosdisk part_msdos"
-sed -i 's/\/dev\/nbd0p1/\/dev\/sda1/g' /boot/grub/grub.cfg
+
+grub-install /dev/nbd0 --modules="biosdisk part_msdos" --force || true
+grub-mkconfig -o /boot/grub/grub.cfg
+sed -i 's/\/dev\/nbd0p1/\/re/g' /boot/grub/grub.cfg
 sync
 EOF
 
@@ -73,7 +79,6 @@ mount -v --bind /dev /mnt/debian_7/dev
 mount -vt proc proc /mnt/debian_7/proc
 mount -vt sysfs sysfs /mnt/debian_7/sys
 mount -vt tmpfs tmpfs /mnt/debian_7/run
-
 
 chroot /mnt/debian_7 /bin/bash /root/postinst.sh
 chroot /mnt/debian_7 /bin/bash -c "rm -rf /root/postinst.sh"
