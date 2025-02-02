7eb07e9ab95f7489777e5a6dd0753d58731695f5
Alexey Veselov <ukiroot@gmail.com>
2025-02-02 23:01:18 +0000

    add 'pytest-dependency' dependency between tests

--- 7_devops/bootstrap/debian_like/debian/7_wheezy/test_bootstrap_debian7_with_debootstrap_ndb.py
+++ 7_devops/bootstrap/debian_like/debian/7_wheezy/test_bootstrap_debian7_with_debootstrap_ndb.py
@@ -5,5 +5,10 @@ import base
 
 
 @pytest.mark.bootstrap
+@pytest.mark.dependency(name="deploy_debian7")
 def test_bash_script():
     base.bash_script(os.path.dirname(__file__))
+
+@pytest.mark.dependency(depends=["deploy_debian7"])
+def test_smoke_run():
+    assert True
